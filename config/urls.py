012085a3a334c72828ef45c8b8315d9f3628a2a4
aselkg <aselekkg@gmail.com>
2020-03-23 21:42:28 -0400

    basic nav bar and homepage

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
+from eTutor import views as etutor_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +25,7 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
+        path('', etutor_views.homepage, name="homepage"),
         path('__debug__/', include(debug_toolbar.urls)),
 
         # For django versions before 2.0:
