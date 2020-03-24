a7cbe9d0cb0633e2dade6944dcb53ca8e63f98b8
dandeboer <ddeboer926@gmail.com>
2020-03-24 14:17:42 -0400

    can get data from a form that is ready to send via ajax

--- static/js/messaging.js
+++ static/js/messaging.js
@@ -1,1 +1,10 @@
 console.log("hello")
+let messageForm = document.querySelector('#message-form')
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    let recipientInput = document.querySelector('#recipient-input')
+    let messageInput = document.querySelector('#message-input')
+    let data = {recipient: recipientInput.value, message: messageInput.value}
+    console.log(data)
+})
