068b9a6a30e857be91d56cf89b36992cae353695
dandeboer <ddeboer926@gmail.com>
2020-03-24 12:18:12 -0400

    commented out custom user models until language models are added. app can now run localhost

--- users/models.py
+++ users/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from eTutor.models import Language
+# from eTutor.models import Language
 # Consider creating a custom user model from scratch as detailed at
 # https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#specifying-a-custom-user-model
 
 
 class User(AbstractUser):
-    primary_language = models.ForeignKey(to=Language, on_delete=models.DO_NOTHING,)
-    known_languages = models.ForeignKey(to=Language, on_delete=models.DO_NOTHING)
-    wanted_languages = models.ForeignKey(to=Language, on_delete=models.DO_NOTHING)
-    current_time_zone = models.CharField(max_length=100)
-
+    # primary_language = models.ForeignKey(to=Language, on_delete=models.DO_NOTHING,)
+    # known_languages = models.ForeignKey(to=Language, on_delete=models.DO_NOTHING)
+    # wanted_languages = models.ForeignKey(to=Language, on_delete=models.DO_NOTHING)
+    # current_time_zone = models.CharField(max_length=100
+    pass
