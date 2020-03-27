1bcb0e3e5f975e7017ba11ccbcb86720014fbcdd
aselkg <aselekkg@gmail.com>
2020-03-27 15:43:50 -0400

    registration page

--- users/forms.py
+++ users/forms.py
@@ -3,12 +3,7 @@ from registration.forms import RegistrationForm
 from .models import User
 
 class CustomRegistrationForm(RegistrationForm):
-    first_name = forms.CharField(max_length=30, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length=30, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(max_length=254, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control',
-       
 
     class Meta(RegistrationForm.Meta):
         model = User
-        fields = fields = ['username', 'first_name', 'last_name', 'email']
+        fields = RegistrationForm.Meta.fields + ('primary_language',)
